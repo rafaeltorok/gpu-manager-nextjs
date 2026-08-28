f9421945fe257e693a766f3f45d38b779a751a15
rafaeltorok <rafaeltorok@gmail.com>
2026-08-28 15:11:52 -0300

    fix: the setup config for the Vitest integration tests - add the proper env variable file path to the dotenv configuration - fix: minor syntax issues on some code comments

--- tests/integration/setup.ts
+++ tests/integration/setup.ts
@@ -2,6 +2,9 @@
 import { beforeAll, afterAll } from "vitest";
 import mongoose from "mongoose";
 import connectToDatabase from "../../lib/mongodb";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 beforeAll(async () => {
   await connectToDatabase();
