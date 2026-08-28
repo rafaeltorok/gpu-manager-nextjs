3e69ad34742fa29f28c1b0b18c8e2eff4e73d0bd
rafaeltorok <rafaeltorok@gmail.com>
2026-08-28 11:35:50 -0300

    fix: access related issues for local network devices

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allows local devices access to the onClick handler on the NavBar
+  // This is required for the hamburger-style menu to work while on dev mode
+  allowedDevOrigins: ["192.168.0.*:3000", "192.168.0.*", "0.0.0.0", "0.0.0.0:3000"],
 };
 
 export default nextConfig;
