929d51ea3e179c0597073570c18f68a308b97fbb
Rafael Torok <rafaeltorok@gmail.com>
2026-09-12 14:54:22 -0300

    update(ui): implement a new calculate mode to the data table - refactor: exchange the original gpu data for a copy of it - opt for a unified state on the parent component for the GpuTable data - add a wrapper element for the table controls - add more comments to clarify the code refactor changes

--- components/GpuTableRow.tsx
+++ components/GpuTableRow.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { useState } from "react";
-
 // TypeScript types
+import type { GpuType } from "@/types/gpu";
+
 interface ComponentProps {
   header: string;
   data: string | number;
   gpuClass: string;
   editMode: boolean;
-  originalValue: number;
-  name: string;
+  calculateMode?: boolean;
+  originalData: GpuType;
+  name: keyof GpuType;
+  setData: (gpuData: GpuType) => void;
 }
 
 // Client component
@@ -18,14 +20,14 @@ export default function GpuTableRow({
   data,
   gpuClass,
   editMode,
-  originalValue,
+  calculateMode,
+  originalData,
   name,
+  setData,
 }: ComponentProps) {
-  const [value, setValue] = useState<number>(originalValue);
-
   return (
     <>
-      {editMode ? (
+      {editMode || calculateMode ? (
         <div className="flex w-full sm:flex-1">
           <div className="text-left font-normal bg-[#252525] border-1 border-gray-600 px-2 py-1 w-2/5">
             {header}
@@ -36,9 +38,13 @@ export default function GpuTableRow({
             <input
               className="w-full bg-black text-white border-[inset]"
               name={name}
-              value={value}
+              value={originalData[name]}
               type="number"
-              onChange={(e) => setValue(Number(e.target.value))}
+              onChange={
+                (e) => {
+                  setData({ ...originalData, [name]: Number(e.target.value) });
+                }
+              }
             />
           </div>
         </div>
