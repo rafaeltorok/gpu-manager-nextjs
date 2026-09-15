7f940056145b0de6fe0729a7367e060347c4a9fc
rafaeltorok <rafaeltorok@gmail.com>
2026-09-15 16:18:31 -0300

    fix: incorrect TS type for the updateSpecs service

--- services/gpus.ts
+++ services/gpus.ts
@@ -6,7 +6,7 @@ import connectToDatabase from "@/lib/mongodb";
 import { mapSlugs } from "../utils/slug";
 
 // TypeScript types
-import type { NewGpu, GpuType, EditGpu } from "../types/gpu";
+import type { NewGpu, GpuType } from "../types/gpu";
 
 // Fetch all cards from the database
 export async function getGpus() {
@@ -34,7 +34,7 @@ export async function addGpu(data: NewGpu): Promise<GpuType | null> {
   return storedGpu;
 }
 
-export async function updateSpecs(gpu: EditGpu): Promise<GpuType | null> {
+export async function updateSpecs(gpu: GpuType): Promise<GpuType | null> {
   const updatedGpu = await Gpu.findByIdAndUpdate(gpu.id, gpu, {
     new: true,
     runValidators: true,
