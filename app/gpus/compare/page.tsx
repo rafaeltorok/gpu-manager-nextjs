472724466247321e4cfb0fc333b17b0945923a6d
rafaeltorok <rafaeltorok@gmail.com>
2026-09-18 18:39:22 -0300

    fix: stale clock speed values when selecting the comparison cards - implement a key value inside of the ComparisonTable component to force React to re-render the table wit the updated clock speeds

--- app/gpus/compare/page.tsx
+++ app/gpus/compare/page.tsx
@@ -38,7 +38,9 @@ export default async function Page(props: {
     <div>
       <Selection gpus={mappedSlugs} />
 
+      {/* The key forces React to update the clock speed values after a selection */}
       <ComparisonTable
+        key={`${firstSlug}-${secondSlug}`}
         firstGpuData={firstGpuData}
         secondGpuData={secondGpuData}
       />
