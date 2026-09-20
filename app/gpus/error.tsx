f06cc77226edbccc06dd1e16d48ed558adc88516
Rafael Torok <rafaeltorok@gmail.com>
2026-09-20 11:39:47 -0300

    fix: code formatting issues using Prettier

--- app/gpus/error.tsx
+++ app/gpus/error.tsx
@@ -18,9 +18,7 @@ export default function Error({
       <h2 className="text-center">Something went wrong!</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          () => reset()
-        }
+        onClick={() => reset()}
       >
         Try again
       </button>
